c3979a59699ff128699490c380a7fedd14f1b3e0
AntonioRogel <arogel@typrefrigeracion.com>
2016-04-08 18:52:38 +0000

    [REF] openacademy: Add partner view inherit and model inherit

--- openacademy/__openerp__.py
+++ openacademy/__openerp__.py
@@ -20,7 +20,8 @@
         # 'security/ir.model.access.csv',
         # 'templates.xml',
         'view/openacademy_course_view.xml',
-        'view/openacademy_session_view.xml'
+        'view/openacademy_session_view.xml',
+        'view/partner_view.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
